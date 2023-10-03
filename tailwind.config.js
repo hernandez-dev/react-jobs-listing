2a4dd2bab1f08761428e3de0dd37783a37383dd8
hernandez maikol <hernandez_maikol@protonmail.com>
2023-10-03 14:44:28 -0400

    Project - tailwind - extending theme ( colors )

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "hsl(180, 52%, 96%)",
+        "light-cyan": "hsl(180, 31%, 95%)",
+        "desaturated-dark-cyan": "hsl(180, 29%, 50%)",
+        "dark-grayish-cyan": "hsl(180, 8%, 52%)",
+        "very-dark-grayish-cyan": "hsl(180, 14%, 20%)"
+      }
+    },
   },
   plugins: [],
 }
