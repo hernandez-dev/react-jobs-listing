6bc3a290702b6255e8e9951f9d2a7fd2899e528f
hernandez maikol <hernandez_maikol@protonmail.com>
2023-10-03 20:45:23 -0400

    Components - JobPost - dispatching app event ( insert-filter-languages )

--- src/components/JobPost.jsx
+++ src/components/JobPost.jsx
@@ -1,10 +1,47 @@
+import { useState, useEffect, useContext } from "react"
+
+// context
+import AppState from "../context/AppState.js"
+import AppDispatch from "../context/AppDispatch.js"
+
 export default function JobPost({ job }) {
-  return(
-    <article className={`relative p-5 bg-white mt-16 border-l-4 ${job.featured ? 'border-desaturated-dark-cyan' : 'border-white'} rounded-lg shadow-lg shadow-desaturated-dark-cyan/30 lg:flex lg:items-center lg:p-8 lg:mt-10`}>
-      <div className="hidden lg:block lg:w-20">
-        <img
-          src={job.logo}
-          alt={job.name}
+  // component state
+  const [count, setCount] = useState(0)
+
+  // app state
+  const { languages } = useContext(AppState)
+
+  // app dispatch
+  const appDispatch = useContext(AppDispatch)
+
+  // handleLanguageClick
+  function handleLanguageClick(language) {
+    const languageExist = languages.find(prev => prev.toLowerCase() == language)
+    if (!languageExist) {
+      appDispatch({ type: "insert-filter-language", value: language })
+    }
+  }
+
+  // languages changes
+  useEffect(() => {
+    if (languages.length) {
+      for (const language of languages) {
+        if (job.languages.find(prev => prev.toLowerCase() == language.toLowerCase())) {
+          setCount(prev => prev += 1)
+        }
+      } // for of end
+    } else {
+      setCount(1)
+    }
+    return () => setCount(0)
+}, [languages])
+
+return(
+  <article className={`relative p-5 bg-white mt-16 first:mt-0 border-l-4 ${job.featured ? 'border-desaturated-dark-cyan' : 'border-white'} rounded-lg shadow-lg shadow-desaturated-dark-cyan/30 ${count ? 'lg:flex lg:items-center lg:p-8 lg:mt-10' : 'hidden'}`}>
+    <div className="hidden lg:block lg:w-20">
+      <img
+        src={job.logo}
+    alt={job.name}
           className="block w-full"
         />
       </div>
@@ -37,6 +74,7 @@ export default function JobPost({ job }) {
           <button
             key={language}
             className="inline-block bg-primary px-4 ml-4 first:ml-0 rounded-md font-bold text-sm text-center text-desaturated-dark-cyan leading-10 transition duration-100 hover:bg-desaturated-dark-cyan hover:text-primary"
+            onClick={() => handleLanguageClick(language.toLowerCase())}
           >
             {language}
           </button>
